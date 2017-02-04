7eb6b9fae5e52c95aa962a323faa79197b766876
Fenykepy <pro@lavilotte-rolle.fr>
2017-02-04 08:27:09 +0100

    remove unuseful sitemaps

--- backend/pinpict/urls.py
+++ backend/pinpict/urls.py
@@ -1,20 +1,9 @@
 from django.conf.urls import include, url
 
-from django.contrib import admin
-from django.contrib.auth.decorators import login_required
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 
-from pinpict.sitemaps import UserSitemap, BoardSitemap, PinSitemap
-
-sitemaps = {
-    'users': UserSitemap,
-    'boards': BoardSitemap,
-    'pins': PinSitemap,
-}
 
 
 urlpatterns = [
@@ -27,11 +16,5 @@ urlpatterns = [
     url(r'^api/users/', include('user.urls')), # users API
     url(r'^api/pin/', include('pin.urls')),
 
-    ## site map
-    url(r'^sitemap\.xml$', sitemap,
-        {'sitemaps': sitemaps}),
 ]
 
-# To get static files during development
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
