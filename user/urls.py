8457c253a0eabf68bd9bb87f0fc0649ba0c64aa8
Fenykepy <pro@lavilotte-rolle.fr>
2019-11-03 15:58:23 +0100

    Fix a bug in user urls.

--- user/urls.py
+++ user/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
         url(r'^current/$', views.CurrentUserDetail.as_view(),
             name="current-user"),
         url(r'^menu/$', views.user_root, name='user-root'),
-        url(r'(?P<slug>[-\w]+)/$', views.UserDetail.as_view(),
-            name="user-detail"),
         url(r'(?P<slug>[-\w]+)/public/$', views.PublicUserDetail.as_view(),
             name="public-user"),
+        url(r'(?P<slug>[-\w]+)/$', views.UserDetail.as_view(),
+            name="user-detail"),
         url(r'^$', views.UserList.as_view(),
             name="user-list"),
 ]
